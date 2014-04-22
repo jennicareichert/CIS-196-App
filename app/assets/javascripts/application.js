83722d8197b6f42ba2533ab6ffe63399642032c6
Jennica Reichert <jennica@wharton.upenn.edu>
2014-04-21 22:06:25 -0400

    Homework #10

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,11 @@
 
 $(function(){
 	setHeight();
+	setWidth();
 
 	$(window).on("resize", function(){
 		setHeight();
+		setWidth();
 	});
 
 	function setHeight(){
@@ -30,5 +32,49 @@ $(function(){
 		$("#wrapper").height(wrapperHeight);
 		//$("#wrapper").offset(30).bottom; //returns height of div with id = "wrapper"
 		//var navSize = $(".nav").height(50), $(".nav").width(1382), $(".nav").offset(8).top, $(".nav").offset(8).left; 
+	}
+
+	function setWidth(){
+		console.log("here");
+		if ($(window).width() <= 1010) 
+		{
+			$(".nav .dropdown").removeClass("hide");
+			$(".dashboard").addClass("hide");
+		}else{
+			$(".nav .dropdown").addClass("hide");
+			$(".dashboard").removeClass("hide");
+		}
+	}
+});
+
+$(document).on("page:load", function(){
+	setHeight();
+	setWidth();
+
+	$(window).on("resize", function(){
+		setHeight();
+		setWidth();
+	});
+
+	function setHeight(){
+		//$(selectors).methodName()
+		var windowHeight = $(window).height();
+		var distFromTop = $("#wrapper").offset().top;
+		var wrapperHeight = windowHeight - distFromTop - 30;
+		$("#wrapper").height(wrapperHeight);
+		//$("#wrapper").offset(30).bottom; //returns height of div with id = "wrapper"
+		//var navSize = $(".nav").height(50), $(".nav").width(1382), $(".nav").offset(8).top, $(".nav").offset(8).left; 
+	}
+
+	function setWidth(){
+		console.log("here");
+		if ($(window).width() <= 1010) 
+		{
+			$(".nav .dropdown").removeClass("hide");
+			$(".dashboard").addClass("hide");
+		}else{
+			$(".nav .dropdown").addClass("hide");
+			$(".dashboard").removeClass("hide");
+		}
 	}
 });
